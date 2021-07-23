9299b7be71b17a6d90657e76169acfbe8a6eefa5
Arsen Girinov <amateurlib@yandex.ru>
2021-07-23 22:42:21 +0300

    init git

---
+++ src/main/java/com/github/arsengir/conditional/ConditionalApplication.java
@@ -0,0 +1,13 @@
+package com.github.arsengir.conditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/arsengir/conditional/ConditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arsengir.conditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
